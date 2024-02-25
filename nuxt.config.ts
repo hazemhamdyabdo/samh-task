229c9b4ce58c843ae3c28abd9fd25965b5f5f82a
Hazem <hazemhamdy11@gmail.com>
2024-02-25 15:56:48 +0200

    intail commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "nuxt-lucide-icons",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    vueI18n: "./i18n.config.ts", // if you are using custom path, default
+  },
+  tailwindcss: {
+    // Options
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
